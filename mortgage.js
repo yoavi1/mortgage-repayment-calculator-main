6c279e72e2a66901ac3ed0a31cd137be7d1fca28
yoavi1 <yoavmeirovitz187@gmail.com>
2024-07-29 09:22:16 +0300

    new v

--- mortgage.js
+++ mortgage.js
@@ -1,13 +1,123 @@
 const mortagageAmount = document.querySelector('#Mortgage-Amount');
 const mortgageTerm = document.querySelector('#Mortgage-Term');
 const interestRate = document.querySelector('#Interest-Rate');
+const numberPaymentsPerYear = 12;
+// const repaymentRadioOption = document.querySelector('#repayment-option');
+// const interestOnlyRadioOption = document.querySelector('#interest-only-option');
+const radioOptionsElement = document.querySelectorAll(
+  'input[name="radio-options"]'
+);
 // console.log(mortagageAmount);
 const btn = document.querySelector('#btn');
 // const calcBtn = document.querySelector('.calculator-btn');
 
-btn.addEventListener('click', function (e) {
-  e.preventDefault();
-  let mortagageAmountValue = Number(mortagageAmount.value);
+btn.addEventListener('click', calculate);
 
-  console.log(mortagageAmountValue);
-});
+function calculate(event) {
+  event.preventDefault();
+
+  // console.log(CheckedRadioOption());
+
+  if (!CheckedRadioOption()) {
+    return;
+  }
+  const mortagageAmountValue = Number(mortagageAmount.value);
+  const mortgageTermValue = Number(mortgageTerm.value);
+  const interestRateValue = Number(interestRate.value) / 100;
+  const monthlyRepayment = calculateMonthlyRepayment(
+    mortagageAmountValue,
+    mortgageTermValue,
+    interestRateValue
+  );
+  const totalRepayment = calculateTotalRepayment(
+    monthlyRepayment,
+    mortgageTermValue
+  );
+  const MonthlyInterestOnlyRepayment = calculateMonthlyInterestOnlyRepayment(
+    mortagageAmountValue,
+    interestRateValue
+  );
+  const TotalInterestOnlyRepayment = calculateTotalInterestOnlyRepayment(
+    mortagageAmountValue,
+    interestRateValue,
+    mortgageTermValue
+  );
+
+  switch (CheckedRadioOption()) {
+    case 'repayment':
+      console.log('monthlyRepayment', monthlyRepayment);
+      console.log('total repayment', totalRepayment);
+      break;
+    case 'interest-only':
+      console.log(
+        'Monthly Interest Only Repayment',
+        MonthlyInterestOnlyRepayment
+      );
+      console.log('Total Interest Only Repayment', TotalInterestOnlyRepayment);
+      break;
+  }
+
+  // console.log('monthlyRepayment', monthlyRepayment);
+  // console.log('total repayment', totalRepayment);
+  // console.log('Monthly Interest Only Repayment', MonthlyInterestOnlyRepayment);
+  // console.log('Total Interest Only Repayment', TotalInterestOnlyRepayment);
+}
+function calculateMonthlyRepayment(
+  mortagageAmountValue,
+  mortgageTermValue,
+  interestRateValue
+) {
+  return (
+    (mortagageAmountValue * interestRateValue) /
+    numberPaymentsPerYear /
+    (1 -
+      Math.pow(
+        1 + interestRateValue / numberPaymentsPerYear,
+        -numberPaymentsPerYear * mortgageTermValue
+      ))
+  );
+}
+
+function calculateTotalRepayment(monthlyRepayment, mortgageTermValue) {
+  return monthlyRepayment * numberPaymentsPerYear * mortgageTermValue;
+}
+
+function calculateMonthlyInterestOnlyRepayment(
+  mortagageAmountValue,
+  interestRateValue
+) {
+  return mortagageAmountValue * (interestRateValue / numberPaymentsPerYear);
+}
+
+function calculateTotalInterestOnlyRepayment(
+  mortagageAmountValue,
+  interestRateValue,
+  mortgageTermValue
+) {
+  return mortagageAmountValue * interestRateValue * mortgageTermValue;
+}
+
+//return the radio option that checked by the user if  the user didnt
+//checked any option return null.
+function CheckedRadioOption() {
+  let choise;
+  radioOptionsElement.forEach((option) => {
+    if (option.checked) {
+      choise = option.value;
+      return;
+    }
+  });
+  return choise;
+  // return null;
+}
+
+function isValidForm(params) {}
+// M is the monthly mortgage payment.
+// 𝑃
+// P is the principal loan amount.
+// 𝑟
+// r is the annual interest rate (in decimal form; e.g., 5% is 0.05).
+// 𝑛
+// n is the number of payments per year (12 for monthly payments).
+// 𝑡
+// t is the loan term in years.
